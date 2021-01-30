e85f507b97d4ac873d23fec01dcfdd9ac9a0f3fc
Samira <54632872+S-samira2020@users.noreply.github.com>
2021-01-30 21:09:08 +0600

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,21 @@
+#MATH fUNCTION PRACTICE
+
+'''x  = 2.9
+print(round(x))
+print()
+
+x = 3.8
+print(abs(-3.8))
+print()
+'''
+
+import math
+print(math.ceil(2.1)) # it will show 3 bcz ceil means it is up 2.0
+print(math.floor(2.9)) # it will show 2 bcz floor means under 3
+print(math.comb(6,3)) # it will show 20 bcz n!/k!+(n-k)! here n is 6 and 3 is k
+print(math.copysign(6.1, -5.9)) # it will show -6.1 it is only copy y's sign i mean minus sign
+x  = 3
+print(math.fabs(x))# it will show exact value of x 3.5
+print(math.factorial(x)) # it will show 6
+print(math.fmod(2.0,1.0)) # it will show 0.0
+print(math.fmod(2.0, 6.0)) # it will show 2.0
